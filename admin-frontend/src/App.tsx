98f2cf2fa9ed4ff437b1a97f51627a6509d84d70
Spyro <redstone.mc01@gmail.com>
2025-11-17 17:56:30 +0100

    Admin-frontend react project added

---
+++ admin-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>KareoQ Admin Frontend</h1>
+      <p>React alkalmazás alap nézettel</p>
+    </div>
+  );
+}
